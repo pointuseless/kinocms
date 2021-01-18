c9b271b75961f8c4f234e410b9ff86ac4036665f
pointuseless <s3m.channel@gmail.com>
2021-01-19 00:02:37 +0200

    trying to figure out what to do with business-logic. again.

---
+++ kinocms/movies/cinema.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+from datetime import timedelta, datetime
+from functools import singledispatchmethod
+
+
+class Movie:
+
+    def __init__(self, title: str, duration: timedelta):
+        self.title = title
+        self.duration = duration
+
+
+# Возможно, место вообще не нужно. Row может хранить список мест со значение True, False
+class Place:
+
+    def __init__(self, identification: int):
+        self.__occupied = False
+        self.id = identification
+
+    def occupy(self):
+        self.__occupied = True
+
+    def free(self):
+        self.__occupied = False
+
+    def clone(self) -> Place:
+        return Place(self.id)
+
+
+class Row:
+
+    def __init__(self, identification: int, places: list[Place]):
+        self.id = identification
+        self.places = places
+
+    # def list_places(self):
+    #     return self.places
+
+    def clone(self) -> Row:
+        places = [place.clone() for place in self.places]
+        return Row(self.id, places)
+
+    def __iter__(self):
+        return iter(self.places)
+
+
+class Sector:
+
+    def __init__(self, rows: list[Row], row_type: str):
+        self.rows = rows
+        self.type = row_type
+
+    # def list_places(self):
+    #     places = [row.list_places() for row in self.rows]
+    #     return places
+
+    def clone(self) -> Sector:
+        rows = [row.clone() for row in self.rows]
+        return Sector(rows, self.type)
+
+    def __iter__(self):
+        return iter(self.rows)
+
+
+class Hall:
+
+    def __init__(self, sectors: list[Sector], technology: str):
+        self.sectors = sectors
+        self.technology = technology
+
+    # def list_places(self):
+    #     places = [sector.list_places() for sector in self.sectors]
+    #     return places
+
+    def clone(self) -> Hall:
+        sectors = [sector.clone() for sector in self.sectors]
+        return Hall(sectors, self.technology)
+
+    def __iter__(self):
+        return CompositeIterator(self.sectors)
+
+
+class PriceList:
+
+    def __init__(self, cheap: int, medium: int, vip: int):
+        self.cheap = cheap
+        self.medium = medium
+        self.vip = vip
+
+    def sector_price(self, sector: Sector):
+        if sector.type == 'CHEAP':
+            return self.cheap
+        elif sector.type == 'MEDIUM':
+            return self.medium
+        elif sector.type == 'VIP':
+            return self.vip
+
+
+class CompositeIterator:
+
+    def __init__(self, composite):
+        self.nested = self.make_nested(composite)
+        self.current = 0
+
+    @singledispatchmethod
+    def make_nested(self, composite):
+        return [CompositeIterator(component) for component in composite]
+
+    @make_nested.register(Row)
+    def _(self, composite):
+        return [iter(composite)]
+
+    def __next__(self):
+        try:
+            return next(self.nested[self.current])
+        except StopIteration:
+            self.current += 1
+            return self.__next__()
+        except IndexError:
+            raise StopIteration
+
+
+class Show:
+
+    def __init__(self, movie: Movie, hall: Hall, showtime: datetime, price_list: PriceList):
+        self.movie = movie
+        self.hall = hall.clone()
+        self.showtime = showtime
+        self.price_list = price_list
+
+    # def list_places(self) -> list[Place]:
+    #     return self.hall.list_places()
+
+    def __iter__(self):
+        return iter(self.hall)
+
+
+places = [[Place(i * 100 + j) for j in range(1, 10)] for i in range(1, 10)]
+rows = [Row(i + 1, places[i]) for i in range(9)]
+sectors = [Sector(rows[: 3], 'CHEAP'), Sector(rows[3: 8], 'MEDIUM'), Sector(rows[8:], 'VIP')]
+hall = Hall(sectors, 'Dolby Digital')
+
+movie = Movie('Super-Hero', timedelta(hours=1, minutes=55))
+
+price_list = PriceList(50, 70, 90)
+
+show = Show(movie, hall, datetime(2021, 2, 12, 14, 30), price_list)
+
+for place in show:
+    print(place.id)
--- kinocms/movies/models.py
+++ kinocms/movies/models.py
@@ -4,6 +4,22 @@ from datetime import datetime, timedelta
 from django.db import models
 
 
+class Movie(models.Model):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Либо тикету (можно его как отображение представить), давать уже готовую для него инфу
 # А создавать тикет с пережеванной инфой Фабрикой, например
 # Фабрика же может заполнять билет доп инфо, например адресом кинотеатра, поздравлением с нг (новогодняя фабркиа) и тп.
@@ -47,100 +63,4 @@ class PaySystemInterface:
         raise NotImplementedError
 
 
-class Theater:
 
-    def __init__(self, name: str,
-                 address: str,
-                 halls: list[Hall]):
-        self.name = name
-        self.address = address
-        self.halls = halls
-
-
-# Использование этого класса под вопросом
-class Technology:
-
-    def __init__(self, name: str, basic_price: int):
-        self.name = name
-        self.basic_price = basic_price
-
-
-# TODO: типы залов 3D, 2D, etc, с возможностью легко добавить новую технологию
-# Возможно, Медиатором ходить по всем участникам сеанса и считать цену... Только если автоматически!
-# Использование поддержки технологий под вопросом
-# TODO: наверное, сделать композит и клонировать в объект сеанса...
-class Hall:
-
-    def __init__(self, technology: Technology,
-                 rows: list[Row],
-                 works: bool = False):
-        self.technology = technology
-        self.rows = rows
-        self.works = works
-
-    def clone(self) -> Hall:
-        pass
-
-
-class ShowFactory:
-
-    @staticmethod
-    def create_show(movie: Movie, hall: Hall, when: datetime) -> Show:
-        if hall.technology not in movie.supported_technologies:
-            raise RuntimeError('This movie cannot be shown using this technology. Please choose appropriate hall')
-        else:
-            return Show(movie, hall.clone(), when)
-
-
-# Использование price ratio под вопросом - цены могут устанавливаться полностью вручную
-# Использование visible под вопросом - может не понадобиться
-# TODO: Кто-то должен проверять, что фильм будет показан в зале с нужной технологией и наоборот
-# TODO: Кто-то должен проверять, что сеанс будет создан не раньше премьеры
-# TODO: Кто-то должен проверять, что сеанс будет создан не в прошлом
-# TODO: Кто-то должен проверять, что сеанс будет создан в рамках расписания
-class Show:
-
-    def __init__(self, movie: Movie,
-                 hall: Hall,
-                 when: datetime,
-                 price_ratio: float = 1.0,
-                 visible: bool = True):
-        self.movie = movie
-        self.hall = hall
-        self.when = when
-
-
-class Row:
-
-    def __init__(self, places: list[Place]):
-        self.places = places
-
-
-# Наличие отдельного класса под вопросом
-class TemporalReservation:
-
-    def __init__(self, place: Place, timer: timedelta):
-        self.place = place
-        self.timer = timer
-
-
-class Place:
-
-    def __init__(self):
-        self.free = True
-
-    def occupy(self) -> None:
-        self.free = False
-
-
-# Использование поддержки технологий под вопросом
-class Movie:
-
-    def __init__(self, title: str,
-                 duration: timedelta,
-                 supported_technologies: list[Technology],
-                 description: str = ''):
-        self.title = title
-        self.duration = duration
-        self.supported_technologies = supported_technologies
-        self.description = description
